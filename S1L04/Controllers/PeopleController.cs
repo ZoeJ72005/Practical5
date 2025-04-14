9ddc1c17b0ae4dbf8a6f5552a8013433cbd48f15
zoejo <zoejo@Z>
2025-04-14 23:06:53 +0200

    Add project files.

---
+++ S1L04/Controllers/PeopleController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using S1L04.Models;
+
+namespace S1L04.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // Static list to store people
+        private static List<PersonModel> people = new List<PersonModel>();
+
+        // GET: People
+        public ActionResult ListPeople()
+        {
+            return View(people);
+        }
+
+        // GET: Create new person
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Add a new person
+        [HttpPost]
+        public ActionResult AddPerson(PersonModel person)
+        {
+            if (ModelState.IsValid)
+            {
+                people.Add(person); // Add the new person to the list
+            }
+            return RedirectToAction("ListPeople"); // Redirect to refresh the view
+        }
+
+        // POST: Delete a person by index
+        [HttpPost]
+        public ActionResult DeletePerson(int index)
+        {
+            if (index >= 0 && index < people.Count)
+            {
+                people.RemoveAt(index); // Remove the person at the specified index
+            }
+            return RedirectToAction("ListPeople"); // Redirect to refresh the view
+        }
+    }
+}
